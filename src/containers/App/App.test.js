f364026b32801d74e8874a6fdfc5a126a42a7b88
Michael Krog <michaelryankrog@gmail.com>
2019-08-09 14:07:52 -0600

    added readme

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,89 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount } from 'enzyme'
+import { App, mapStateToProps, mapDispatchToProps } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { fetchAllOrders } from '../../thunks/fetchAllOrders';
+import { fetchAllNotes } from '../../thunks/fetchAllNotes';
+
+jest.mock('../../thunks/fetchAllOrders');
+jest.mock('../../thunks/fetchAllNotes');
+
+import * as actions from '../../actions/index';
+
+
+const mockNotes = [
+  {
+    id: 1,
+    title: 'Note Title 0101',
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    status: 0,
+    order_id: 9
+  },
+  {
+    id: 2,
+    title: 'Note Title 0102',
+    copy: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
+    status: 0,
+    order_id: 8
+  },
+  {
+    id: 3,
+    title: 'Note Title 0103',
+    copy: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
+    status: 1,
+    order_id: 9
+  }
+]
+
+const mockOrders = [
+  {
+    id: 8,
+    number: 2222,
+  },
+  {
+    id: 9,
+    number: 2222,
+  }
+]
+
+describe('App', () => {
+
+  describe('App Component', () => {
+    let wrapper;
+    let mockfetchAllOrders = jest.fn()
+    let mockfetchAllNotes = jest.fn()
+
+    beforeEach(() => {
+      wrapper = shallow(<App fetchAllOrders={mockfetchAllOrders}
+                             fetchAllNotes={mockfetchAllNotes}
+                             notes={mockNotes}
+                             orders={mockOrders}
+                             />
+                        )
+    })
+
+    it('should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it("should invoke the thunk fetchAllOrders on componentDidMount", async () => {
+      await wrapper.instance().componentDidMount()
+      expect(mockfetchAllOrders).toBeCalled()
+    })
+
+    it("should invoke the thunk fetchAllNotes on componentDidMount", async () => {
+      await wrapper.instance().componentDidMount()
+      expect(mockfetchAllNotes).toBeCalled()
+    })
+
+  });
+
+  describe('mapStateToProps', () => {
+
+  });
+
+  describe('mapDispatchToProps', () => {
+
+  });
+
 });
